7866fdea6b8615279bdacd764d410c262aa4196d
Vishal Chavda <angelvish1698@gmail.com>
2024-08-10 19:59:16 -0400

    added tvshowlist adptor

--- app/src/main/java/ca/georgiancollage/assignment04/TASKShowListAdapter.kt
+++ app/src/main/java/ca/georgiancollage/assignment04/TASKShowListAdapter.kt
@@ -1,7 +1,29 @@
 package ca.georgiancollage.assignment04
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import ca.georgiancollage.assignment04.databinding.TextRowItemsBinding
 
 class TASKShowListAdapter (private val onItemClicked: (TASKShow) -> Unit) :
     ListAdapter<TASKShow, TASKShowViewHolder>(TASKShowComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TASKShowViewHolder {
+        // Inflate the item layout and create a binding object
+        val binding = TextRowItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        // Return a new ViewHolder instance with the binding object
+        return TASKShowViewHolder(binding)
+    }
+
+    // Called by the RecyclerView to display the data at the specified position.
+    override fun onBindViewHolder(holder: TASKShowViewHolder, position: Int) {
+        // Get the TVShow item at the given position
+        val current = getItem(position)
+        // Bind the TVShow item to the ViewHolder
+        holder.bind(current)
+        // Set a click listener on the itemView to handle item clicks
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+    }
 }
