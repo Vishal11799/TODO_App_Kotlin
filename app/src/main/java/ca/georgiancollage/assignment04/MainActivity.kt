c68902ede1c4cfed8550529b3d2c9e07a1f33518
Vishal Chavda <angelvish1698@gmail.com>
2024-08-10 16:45:25 -0400

    addemock data recicler view

--- app/src/main/java/ca/georgiancollage/assignment04/MainActivity.kt
+++ app/src/main/java/ca/georgiancollage/assignment04/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import ca.georgiancollage.assignment04.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
@@ -16,7 +17,21 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+            //array of the mock data
+        val tasks = arrayOf(
+            TASKShow("Finish Project Report", true, "2024-08-12", "14:00", "Work"),
+            TASKShow("Grocery Shopping", false, "2024-08-10", "18:00", "Personal"),
+            TASKShow("Doctor's Appointment", false, "2024-08-11", "09:30", "Health"),
+            TASKShow("Team Meeting", true, "2024-08-09", "10:00", "Work"),
+            TASKShow("Workout Session", false, "2024-08-10", "07:00", "Fitness")
+        )
+            //instance  of firest adptor
+        val firstAdapter = FirstAdapter(tasks)
+// Use view binding to replace findViewById or synthetic properties
+        binding.firstRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = firstAdapter
+        }
 
     }
 }
